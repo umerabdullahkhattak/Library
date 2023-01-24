7399f8426ec1ffce4cb324ae0370a553b1796952
umer <umer.a.khattak@gmail.com>
2023-01-25 02:07:52 +0500

    form on submission now shows book tiles

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let myLibrary = [];
+//Array where all Book Objects are stored. 
+let myLibrary = []; 
+
+
+//Book Object
 
 function Book(name, author, pages, status){
     this.name = name;
@@ -8,15 +12,11 @@ function Book(name, author, pages, status){
     
 }
 
-Book.prototype.info = function(){
-    if(this.status === false){
-        return console.log(this.name + " " + "by " + this.author + "," + this.pages + " pages, " + "not read yet" )
-    }
-    else{
-        return console.log(this.name + " " + "by " + this.author + "," + this.pages + " pages, "+ "read" )
-    }
-}
 
+
+
+
+// Function to add Book to Library
 function addBookToLibrary(Book) {
   myLibrary.push(Book);
 
@@ -26,63 +26,160 @@ function addBookToLibrary(Book) {
 
 
 
-function displayBooks(){
-    for(let x = 0; x < myLibrary.length; x++){
-        myLibrary[x].info();
+
+
+
+
+
+// Initializing all variables to be used in createBookForm()... 
+// outside the function so they can be used outside function
+
+
+let content = document.querySelector(".content");
+let nameField = document.createElement("input");
+let authorField = document.createElement("input");
+let pagesField = document.createElement("input");
+let readingStatusField = document.createElement("input");
+let submitButton = document.createElement("button");
+let formdiv = document.createElement('div');
+formdiv.setAttribute('id','form');
+var form = document.createElement("form");
+let counter = 0;
+
+
+//Function to generate form which would take book information.
+// Function would apply when Add Book button is clicked.
+function createBookForm() {
+    
+       
+    
+        
+        let form = document.createElement("form");
+        form.classList.add("book-form")
+        
+        
+        nameField.classList.add("formfield")
+        nameField.setAttribute("type", "text");
+        nameField.setAttribute("placeholder", "Name");
+        
+        
+        
+        
+        authorField.classList.add("formfield")
+        authorField.setAttribute("type", "text");
+        authorField.setAttribute("placeholder", "Author");
+        
+
+        
+        
+        pagesField.classList.add("formfield")
+        pagesField.setAttribute("type", "text");
+        pagesField.setAttribute("placeholder", "Pages");
+        
+    
+        
+        readingStatusField.classList.add("formfield")
+        readingStatusField.setAttribute("type", "text");
+        readingStatusField.setAttribute("placeholder", "Reading Status");
+        
+        
+        
+        submitButton.innerHTML = "Add"
+        submitButton.classList.add("submitButton")
+        submitButton.setAttribute("type", "submit");
+        submitButton.setAttribute("value", "Submit");
+        
+        
+        
+            
+       
+        
+        form.appendChild(submitButton);
+        form.appendChild(readingStatusField);
+        form.appendChild(pagesField);
+        form.appendChild(authorField);
+        form.appendChild(nameField);
+        
+        formdiv.appendChild(form)
+        
+        
+        content.appendChild(formdiv);
+        
+        
+    
+    
+  }
+ 
+//Calling createBookForm() function on clicking Add Book Button
+  
+let addBookButton = document.querySelector(".addBook");
+addBookButton.addEventListener("click", (e) => {
+    let content = document.querySelector(".content")
+    
+    createBookForm();
+    
+})
+
+
+// Events to happen when submit button of form is clicked. 
+// Events are: Getting data from the form and storing in the myLibrary[] and closing the form
+submitButton.addEventListener('click', (e) => {
+            
+    let bookName = nameField.value;
+    let authorName = authorField.value;
+    let noOfPages = pagesField.value;
+    let readingStatus = readingStatusField.value;
+    let newBook = new Book(bookName, authorName, noOfPages, readingStatus);
+    addBookToLibrary(newBook);
+    console.log(myLibrary)
+    content.removeChild(formdiv)
+    nameField.value = "";
+    authorField.value = "";
+    pagesField.value = "";
+    readingStatusField.value = "";
+    showingBookTiles(counter);
+    counter++;
+    console.log(counter)
+    
+    
+    
+})
+function showingBookTiles(counter){
+    
+   
+    let bookTile = document.createElement('div');
+    
+    for(let x = counter; x< myLibrary.length; x++){
+        let name = myLibrary[x].name;
+        let author = myLibrary[x]. author;
+        let NOP = myLibrary[x].pages;
+        let status = myLibrary[x].status;
+        
+        bookTile.classList.add("bookTile");
+        
+        let tileName = document.createElement("h1");
+        tileName.innerHTML = name;
+        bookTile.appendChild(tileName);
+        
+
+        let tileAuthor = document.createElement("h1");
+        tileAuthor.innerHTML = author;
+        bookTile.appendChild(tileAuthor);
+
+        let tilepages = document.createElement("h1");
+        tilepages.innerHTML = NOP;
+        bookTile.appendChild(tilepages);
+
+        let tileStatus = document.createElement("h1");
+        tileStatus.innerHTML = status;
+        bookTile.appendChild(tileStatus);
+        content.appendChild(bookTile)
+       
     }
 }
 
-function createBookForm() {
-    let content = document.querySelector(".content");
-    
 
-    var form = document.createElement("form");
-    form.classList.add("book-form")
-    
-    var nameField = document.createElement("input");
-    nameField.classList.add("formfield")
-    nameField.setAttribute("type", "text");
-    nameField.setAttribute("placeholder", "Name");
-    
-    
-    var authorField = document.createElement("input");
-    authorField.classList.add("formfield")
-    authorField.setAttribute("type", "text");
-    authorField.setAttribute("placeholder", "Author");
-    
 
-    
-    var pagesField = document.createElement("input");
-    pagesField.classList.add("formfield")
-    pagesField.setAttribute("type", "text");
-    pagesField.setAttribute("placeholder", "Pages");
-    
-  
-    var readingStatusField = document.createElement("input");
-    readingStatusField.classList.add("formfield")
-    readingStatusField.setAttribute("type", "text");
-    readingStatusField.setAttribute("placeholder", "Reading Status");
-    
-    
-    var submitButton = document.createElement("input");
-    submitButton.classList.add("formfield")
-    submitButton.setAttribute("type", "submit");
-    submitButton.setAttribute("value", "Submit");
-    
-    form.appendChild(submitButton);
-    form.appendChild(readingStatusField);
-    form.appendChild(pagesField);
-    form.appendChild(authorField);
-    form.appendChild(nameField);
-    
-    
-    
-    
-    content.appendChild(form);
-  }
-  
-let addBookButton = document.querySelector(".addBook")
-addBookButton.addEventListener("click", (e) => {
-    let content = document.querySelector(".content")
-    createBookForm();
-})
+
+
+
